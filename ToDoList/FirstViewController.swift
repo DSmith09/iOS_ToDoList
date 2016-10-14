34c41d5847a958e63fad77978ca75c5422eb6f13
David <dmsmith@Davids-MacBook-Pro.local>
2016-10-13 23:54:20 -0400

    To Do List App

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -8,16 +8,53 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    // Interface Builder Variables
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Constants and Variables
+    let ITEMS_STRING = "items"
+    var items = [String]()
+    
+    // Set Table View Size
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    // Set Value in Each Cell
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Main")
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // Retrieve Data From Storage on App Load
+        let itemsObject = UserDefaults.standard.object(forKey: ITEMS_STRING)
+        if  let tempItems = itemsObject as? [String] {
+            items = tempItems
+        }
+        // Reload the data in the view
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // If Delete is Selected; Remove the Item from the Array and reload the view; Set the Data Storage
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            items.remove(at: indexPath.row)
+            tableView.reloadData()
+            UserDefaults.standard.set(items, forKey: ITEMS_STRING)
+        }
     }
 
 
--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    var items = [String]()
+    
+    let ITEMS_STRING:String = "items"
+    
+    @IBOutlet weak var itemToAdd: UITextField!
+    
+    @IBAction func addTask(_ sender: AnyObject) {
+        let itemsObject = UserDefaults.standard.object(forKey: ITEMS_STRING)
+        
+        // If there's already an array in storage append to it
+        if  let tempItems = itemsObject as? [String] {
+            items = tempItems
+            items.append(itemToAdd.text!)
+        }
+        // Else create a new array with the data
+        else {
+            items = [itemToAdd.text!]
+        }
+        // Set Dictionary
+        UserDefaults.standard.set(items, forKey: ITEMS_STRING)
+        // Reset Text Field
+        resetTextField()
+    }
+    
+    func resetTextField() {
+        itemToAdd.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // Reset Keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Reset Keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
